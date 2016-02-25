954fde15fac0c7059bf3aa3c39c2e378026d3086
sunht <beyondsht@126.com>
2016-02-25 21:26:29 +0800

    增加 mysql 语句

--- ENode.MySql/MySqlTable.sql
+++ ENode.MySql/MySqlTable.sql
@@ -1,80 +1,172 @@
-﻿CREATE TABLE [dbo].[Command] (
-    [Sequence]                BIGINT IDENTITY (1, 1) NOT NULL,
-    [CommandId]               NVARCHAR (36)          NOT NULL,
-    [AggregateRootId]         NVARCHAR (36)          NULL,
-    [MessagePayload]          NVARCHAR (MAX)         NULL,
-    [MessageTypeName]         NVARCHAR (256)         NOT NULL,
-    [CreatedOn]               DATETIME               NOT NULL,
-    CONSTRAINT [PK_Command] PRIMARY KEY CLUSTERED ([CommandId] ASC)
-)
-GO
-CREATE TABLE [dbo].[EventStream] (
-    [Sequence]                BIGINT IDENTITY (1, 1) NOT NULL,
-    [AggregateRootTypeName]   NVARCHAR (256)         NOT NULL,
-    [AggregateRootId]         NVARCHAR (36)          NOT NULL,
-    [Version]                 INT                    NOT NULL,
-    [CommandId]               NVARCHAR (36)          NOT NULL,
-    [CreatedOn]               DATETIME               NOT NULL,
-    [Events]                  NVARCHAR (MAX)         NOT NULL,
-    CONSTRAINT [PK_EventStream] PRIMARY KEY CLUSTERED ([AggregateRootId] ASC, [Version] ASC)
-)
-GO
-CREATE UNIQUE INDEX [IX_EventStream_AggId_CommandId] ON [dbo].[EventStream] ([AggregateRootId], [CommandId])
-GO
-CREATE TABLE [dbo].[SequenceMessagePublishedVersion] (
-    [Sequence]                BIGINT IDENTITY (1, 1) NOT NULL,
-    [ProcessorName]           NVARCHAR (128)         NOT NULL,
-    [AggregateRootTypeName]   NVARCHAR (256)         NOT NULL,
-    [AggregateRootId]         NVARCHAR (36)          NOT NULL,
-    [PublishedVersion]        INT                    NOT NULL,
-    [CreatedOn]               DATETIME               NOT NULL,
-    CONSTRAINT [PK_SequenceMessagePublishedVersion] PRIMARY KEY CLUSTERED ([ProcessorName] ASC, [AggregateRootId] ASC, [PublishedVersion] ASC)
-)
-GO
-CREATE TABLE [dbo].[MessageHandleRecord] (
-    [Sequence]                  BIGINT IDENTITY (1, 1) NOT NULL,
-    [MessageId]                 NVARCHAR (36)          NOT NULL,
-    [HandlerTypeName]           NVARCHAR (256)         NOT NULL,
-    [MessageTypeName]           NVARCHAR (256)         NOT NULL,
-    [AggregateRootTypeName]     NVARCHAR (256)         NOT NULL,
-    [AggregateRootId]           NVARCHAR (36)          NULL,
-    [Version]                   INT                    NULL,
-    [CreatedOn]                 DATETIME               NOT NULL,
-    CONSTRAINT [PK_MessageHandleRecord] PRIMARY KEY CLUSTERED ([MessageId] ASC, [HandlerTypeName] ASC)
-)
-GO
-CREATE TABLE [dbo].[TwoMessageHandleRecord] (
-    [Sequence]                  BIGINT IDENTITY (1, 1) NOT NULL,
-    [MessageId1]                NVARCHAR (36)          NOT NULL,
-    [MessageId2]                NVARCHAR (36)          NOT NULL,
-    [HandlerTypeName]           NVARCHAR (256)         NOT NULL,
-    [Message1TypeName]          NVARCHAR (256)         NOT NULL,
-    [Message2TypeName]          NVARCHAR (256)         NOT NULL,
-    [AggregateRootTypeName]     NVARCHAR (256)         NOT NULL,
-    [AggregateRootId]           NVARCHAR (36)          NULL,
-    [Version]                   INT                    NULL,
-    [CreatedOn]                 DATETIME               NOT NULL,
-    CONSTRAINT [PK_TwoMessageHandleRecord] PRIMARY KEY CLUSTERED ([MessageId1] ASC, [MessageId2] ASC, [HandlerTypeName] ASC)
-)
-GO
-CREATE TABLE [dbo].[ThreeMessageHandleRecord] (
-    [Sequence]                  BIGINT IDENTITY (1, 1) NOT NULL,
-    [MessageId1]                NVARCHAR (36)          NOT NULL,
-    [MessageId2]                NVARCHAR (36)          NOT NULL,
-    [MessageId3]                NVARCHAR (36)          NOT NULL,
-    [HandlerTypeName]           NVARCHAR (256)         NOT NULL,
-    [Message1TypeName]          NVARCHAR (256)         NOT NULL,
-    [Message2TypeName]          NVARCHAR (256)         NOT NULL,
-    [Message3TypeName]          NVARCHAR (256)         NOT NULL,
-    [AggregateRootTypeName]     NVARCHAR (256)         NOT NULL,
-    [AggregateRootId]           NVARCHAR (36)          NULL,
-    [Version]                   INT                    NULL,
-    [CreatedOn]                 DATETIME               NOT NULL,
-    CONSTRAINT [PK_ThreeMessageHandleRecord] PRIMARY KEY CLUSTERED ([MessageId1] ASC, [MessageId2] ASC, [MessageId3] ASC, [HandlerTypeName] ASC)
-)
-GO
-CREATE TABLE [dbo].[LockKey] (
-    [Name]                   NVARCHAR (128)          NOT NULL,
-    CONSTRAINT [PK_LockKey] PRIMARY KEY CLUSTERED ([Name] ASC)
-)
-GO
+﻿-- MySQL dump 10.13  Distrib 5.6.27, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: enode
+-- ------------------------------------------------------
+-- Server version	5.6.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Command`
+--
+
+DROP TABLE IF EXISTS `Command`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Command` (
+  `Sequence` bigint(20) NOT NULL AUTO_INCREMENT,
+  `CommandId` varchar(36) NOT NULL,
+  `AggregateRootId` varchar(36) DEFAULT NULL,
+  `MessagePayload` longtext,
+  `MessageTypeName` varchar(256) DEFAULT NULL,
+  `CreatedOn` datetime DEFAULT NULL,
+  PRIMARY KEY (`CommandId`),
+  UNIQUE KEY `Sequence_UNIQUE` (`Sequence`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+--
+-- Table structure for table `EventStream`
+--
+
+DROP TABLE IF EXISTS `EventStream`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EventStream` (
+  `Sequence` bigint(20) NOT NULL AUTO_INCREMENT,
+  `AggregateRootTypeName` varchar(256) DEFAULT NULL,
+  `AggregateRootId` varchar(36) NOT NULL,
+  `Version` int(11) NOT NULL,
+  `CommandId` varchar(36) DEFAULT NULL,
+  `CreatedOn` datetime DEFAULT NULL,
+  `Events` longtext,
+  PRIMARY KEY (`AggregateRootId`,`Version`),
+  UNIQUE KEY `Sequence_UNIQUE` (`Sequence`),
+  UNIQUE KEY `IX_EventStream_AggId_CommandId` (`AggregateRootId`,`CommandId`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `LockKey`
+--
+
+DROP TABLE IF EXISTS `LockKey`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `LockKey` (
+  `Name` varchar(128) NOT NULL,
+  PRIMARY KEY (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `MessageHandleRecord`
+--
+
+DROP TABLE IF EXISTS `MessageHandleRecord`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `MessageHandleRecord` (
+  `Sequence` bigint(20) NOT NULL AUTO_INCREMENT,
+  `MessageId` varchar(36) NOT NULL,
+  `HandlerTypeName` varchar(256) NOT NULL,
+  `MessageTypeName` varchar(256) DEFAULT NULL,
+  `AggregateRootTypeName` varchar(256) DEFAULT NULL,
+  `AggregateRootId` varchar(36) DEFAULT NULL,
+  `Version` int(11) DEFAULT NULL,
+  `CreatedOn` datetime DEFAULT NULL,
+  PRIMARY KEY (`Sequence`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `SequenceMessagePublishedVersion`
+--
+
+DROP TABLE IF EXISTS `SequenceMessagePublishedVersion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SequenceMessagePublishedVersion` (
+  `Sequence` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ProcessorName` varchar(128) NOT NULL,
+  `AggregateRootTypeName` varchar(256) DEFAULT NULL,
+  `AggregateRootId` varchar(36) NOT NULL,
+  `PublishedVersion` int(11) NOT NULL,
+  `CreatedOn` datetime DEFAULT NULL,
+  PRIMARY KEY (`ProcessorName`,`AggregateRootId`,`PublishedVersion`),
+  UNIQUE KEY `Sequence_UNIQUE` (`Sequence`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `ThreeMessageHandleRecord`
+--
+
+DROP TABLE IF EXISTS `ThreeMessageHandleRecord`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ThreeMessageHandleRecord` (
+  `Sequence` bigint(20) NOT NULL AUTO_INCREMENT,
+  `MessageId1` varchar(36) NOT NULL,
+  `MessageId2` varchar(36) NOT NULL,
+  `MessageId3` varchar(36) NOT NULL,
+  `HandlerTypeName` varchar(256) NOT NULL,
+  `Message1TypeName` varchar(256) DEFAULT NULL,
+  `Message2TypeName` varchar(256) DEFAULT NULL,
+  `Message3TypeName` varchar(256) DEFAULT NULL,
+  `AggregateRootTypeName` varchar(256) DEFAULT NULL,
+  `AggregateRootId` varchar(36) DEFAULT NULL,
+  `Version` int(11) DEFAULT NULL,
+  `CreatedOn` datetime DEFAULT NULL,
+  PRIMARY KEY (`Sequence`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `TwoMessageHandleRecord`
+--
+
+DROP TABLE IF EXISTS `TwoMessageHandleRecord`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TwoMessageHandleRecord` (
+  `Sequence` bigint(20) NOT NULL AUTO_INCREMENT,
+  `MessageId1` varchar(36) NOT NULL,
+  `MessageId2` varchar(36) NOT NULL,
+  `HandlerTypeName` varchar(256) NOT NULL,
+  `Message1TypeName` varchar(256) DEFAULT NULL,
+  `Message2TypeName` varchar(256) DEFAULT NULL,
+  `AggregateRootTypeName` varchar(256) DEFAULT NULL,
+  `AggregateRootId` varchar(36) DEFAULT NULL,
+  `Version` int(11) DEFAULT NULL,
+  `CreatedOn` datetime DEFAULT NULL,
+  PRIMARY KEY (`Sequence`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-25 21:17:50
